7eaa333c20addedb47d736635172731e2fa870de
mohana <mohana4984@gmail.com>
2023-12-11 02:50:47 -0800

    updated code

--- src/main/java/com/java/CSVParserTest.java
+++ src/main/java/com/java/CSVParserTest.java
@@ -10,13 +10,14 @@ public class CSVParserTest {
 
     @Test
     public void testParseWithValidData() throws IOException {
-        String csvData = "1234567890123456\n4123456789012";
+        String csvData = "5567894523129089\n4123456789012";
         ByteArrayInputStream inputStream = new ByteArrayInputStream(csvData.getBytes());
         CSVParser parser = new CSVParser();
 
         List<CreditCardData> result = parser.parse(inputStream);
+
         assertEquals(2, result.size());
-        assertEquals("1234567890123456", result.get(0).getCardNumber());
+        assertEquals("5567894523129089", result.get(0).getCardNumber());
         assertEquals("MasterCard", result.get(0).getCardType());
         assertEquals("4123456789012", result.get(1).getCardNumber());
         assertEquals("Visa", result.get(1).getCardType());
