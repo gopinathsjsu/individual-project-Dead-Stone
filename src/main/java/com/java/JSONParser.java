bf79da652622dc5b8fc1d4c31c688a8d6dbff64c
mohana <mohana4984@gmail.com>
2023-12-11 02:44:47 -0800

    updated the json

--- src/main/java/com/java/JSONParser.java
+++ src/main/java/com/java/JSONParser.java
@@ -1,23 +1,24 @@
 package com.java;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
 import java.util.*;
 
-
 public class JSONParser implements FileFormatParser {
     @Override
     public List<CreditCardData> parse(InputStream inputStream) throws IOException {
-         ObjectMapper mapper = new ObjectMapper();
-         List<Map<String, String>> list = mapper.readValue(inputStream, new TypeReference<List<Map<String, String>>>() {});
-         List<CreditCardData> dataList = new ArrayList<>();
-         for (Map<String, String> map : list) {
-             String cardNumber = map.get("cardNumber");
-             String cardType = CreditCardFactory.getCreditCardType(cardNumber);
-             dataList.add(new CreditCardData(cardNumber, cardType));
-         }
-         return dataList;
+        ObjectMapper mapper = new ObjectMapper();
+        // Assuming the JSON structure starts with an object containing an array of cards
+        Map<String, List<Map<String, String>>> root = mapper.readValue(inputStream, new TypeReference<Map<String, List<Map<String, String>>>>() {});
+        List<Map<String, String>> list = root.get("cards"); // Assuming "cards" is the key for the array
+        List<CreditCardData> dataList = new ArrayList<>();
+        for (Map<String, String> map : list) {
+            String cardNumber = map.get("cardNumber");
+            String cardType = CreditCardFactory.getCreditCardType(cardNumber);
+            dataList.add(new CreditCardData(cardNumber, cardType));
+        }
+        return dataList;
     }
 }
--- src/main/java/com/java/JSONWriter.java
+++ src/main/java/com/java/JSONWriter.java
@@ -4,10 +4,17 @@ import com.fasterxml.jackson.databind.*;
 import java.io.*;
 import java.util.*;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JSONWriter implements FileFormatWriter {
     @Override
     public void write(List<CreditCardData> creditCardData, OutputStream outputStream) throws IOException {
-         ObjectMapper mapper = new ObjectMapper();
-         mapper.writeValue(outputStream, creditCardData);
+        ObjectMapper mapper = new ObjectMapper();
+        // Wrap the data list in a Map to match the expected JSON structure
+        Map<String, List<CreditCardData>> root = new HashMap<>();
+        root.put("cards", creditCardData);
+        mapper.writeValue(outputStream, root);
     }
 }
+
