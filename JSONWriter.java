71f5bf70969667833c8d517f0366e0923dd26ce7
mohana <mohana4984@gmail.com>
2023-12-10 22:49:29 -0800

    updated code

--- JSONWriter.java
+++ JSONWriter.java
@@ -1,11 +1,11 @@
-import com.fasterxml.jackson.databind.*;
+// import com.fasterxml.jackson.databind.*;
 import java.io.*;
 import java.util.*;
 
 public class JSONWriter implements FileFormatWriter {
     @Override
     public void write(List<CreditCardData> creditCardData, OutputStream outputStream) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(outputStream, creditCardData);
+         ObjectMapper mapper = new ObjectMapper();
+         mapper.writeValue(outputStream, creditCardData);
     }
 }
